ee0ff64580a8a766b77f33f9d9ec19c31d66336c
+ <+>
2023-03-27 16:26:19 -0400

    commit tree 5c2ca25210641b48cee87328bbcede4fc2d60321

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :must_include_clickbait
+
+    CLICKBAIT_OPTIONS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def must_include_clickbait
+        if CLICKBAIT_OPTIONS.none? { |opt| opt.match title }
+            errors.add(:title, "NOT DOING ENOUGH")
+        end
+    end
+    
+
+
+
+
 end
